dad6d3795328e0c28bb7697ed9aeb59df689482e
Jesus Perez <perez.jesus@ufl.edu>
2023-04-13 03:23:15 -0400

    Schema change

--- db/schema.rb
+++ db/schema.rb
@@ -15,49 +15,67 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_12_234157) do
     t.text "author_data"
   end
 
-  create_table "authors", id: false, force: :cascade do |t|
-    t.string "average_rating", limit: 4000
-    t.string "author_id", limit: 4000
-    t.string "text_reviews_count", limit: 4000
-    t.string "name", limit: 4000
-    t.string "ratings_count", limit: 4000
+  create_table "authors", primary_key: "author_id", id: :string, force: :cascade do |t|
+    t.decimal "average_rating", precision: 3, scale: 2
+    t.integer "text_reviews_count", precision: 38
+    t.string "name"
+    t.integer "ratings_count", precision: 38
   end
 
-  create_table "books", id: false, force: :cascade do |t|
-    t.string "isbn", limit: 4000
-    t.string "text_reviews_count", limit: 4000
-    t.string "series", limit: 4000
-    t.string "country_code", limit: 4000
-    t.string "language_code", limit: 4000
-    t.string "asin", limit: 4000
-    t.string "is_ebook", limit: 4000
-    t.string "average_rating", limit: 4000
-    t.string "kindle_asin", limit: 4000
+  create_table "books", primary_key: "book_id", id: :string, force: :cascade do |t|
+    t.string "isbn"
+    t.integer "text_reviews_count", precision: 38
+    t.string "series"
+    t.string "country_code"
+    t.string "language_code"
+    t.string "asin"
+    t.string "is_ebook"
+    t.decimal "average_rating", precision: 3, scale: 2
+    t.string "kindle_asin"
     t.string "description", limit: 4000
-    t.string "format", limit: 4000
-    t.string "link", limit: 4000
-    t.string "publisher", limit: 4000
-    t.string "num_pages", limit: 4000
-    t.string "publication_day", limit: 4000
-    t.string "isbn13", limit: 4000
-    t.string "publication_month", limit: 4000
-    t.string "url", limit: 4000
-    t.string "image_url", limit: 4000
-    t.string "book_id", limit: 4000
-    t.string "ratings_count", limit: 4000
-    t.string "work_id", limit: 4000
-    t.string "title", limit: 4000
-    t.string "title_without_series", limit: 4000
+    t.string "format"
+    t.string "link"
+    t.string "publisher"
+    t.integer "num_pages", precision: 38
+    t.integer "publication_day", precision: 38
+    t.string "isbn13"
+    t.integer "publication_month", precision: 38
+    t.integer "publication_year", precision: 38
+    t.string "url"
+    t.string "image_url"
+    t.integer "ratings_count", precision: 38
+    t.integer "work_id", precision: 38
+    t.string "title"
+    t.string "title_without_series"
   end
 
   create_table "books_json", id: false, force: :cascade do |t|
     t.text "json_data"
   end
 
-  create_table "writes", id: false, force: :cascade do |t|
+  create_table "genres", id: false, force: :cascade do |t|
     t.string "book_id", limit: 4000
-    t.string "id", limit: 4000
-    t.string "rol", limit: 4000
+    t.string "fiction", limit: 4000
+    t.string "history", limit: 4000
+    t.string "fantasy", limit: 4000
+    t.string "mystery", limit: 4000
+    t.string "poetry", limit: 4000
+    t.string "romance", limit: 4000
+    t.string "nonfiction", limit: 4000
+    t.string "comics", limit: 4000
+    t.string "biography", limit: 4000
+    t.string "youngadult", limit: 4000
+    t.string "children", limit: 4000
+  end
+
+  create_table "genres_json", id: false, force: :cascade do |t|
+    t.text "genre_data"
+  end
+
+  create_table "writes", id: false, force: :cascade do |t|
+    t.string "book_id"
+    t.string "author_id"
+    t.string "role"
   end
 
 end
